d4590cb83378806d3e1848d121877f83e5ea58cb
Alberto Larios <albertolarios@me.com>
2023-04-24 16:24:02 -0600

    [IMP] estate: grant full permissions to base.group_user T#71276
    - Specify access to model.
    - Add .act_window action in tree and form mode.
    - Add menu with <menuitem> shortcut.
    - Modify and further specify some fields.
    - Fix inability to record values in DB due to type in default

--- estate/__manifest__.py
+++ estate/__manifest__.py
@@ -5,5 +5,10 @@
     "author": "Vauxoo",
     "depends": [
         "base",
-    ]
+    ],
+    "data": [
+        "security/ir.model.access.csv",
+        "views/estate_property_views.xml",
+        "views/estate_menus.xml",
+    ],
 }
--- estate/models/estate_property.py
+++ estate/models/estate_property.py
@@ -8,18 +8,33 @@ class EstateProperty(models.Model):
     name = fields.Char(required=True)
     description = fields.Text()
     postcode = fields.Char()
-    date_availability = fields.Date()
+    date_availability = fields.Date(copy=False, default=lambda self: fields.Date.add(fields.Date.today(), months=3))
     expected_price = fields.Float(required=True)
-    selling_price = fields.Float()
-    bedrooms = fields.Integer()
+    selling_price = fields.Float(copy=False, readonly=True)
+    bedrooms = fields.Integer(default=2)
     living_area = fields.Integer()
     facades = fields.Integer()
     garage = fields.Boolean()
     garden = fields.Boolean()
     garden_area = fields.Integer()
-    garden_orientation = fields.Selection([
-        ("north", "North"),
-        ("south", "South"),
-        ("east", "East"),
-        ("west", "West")
-    ])
+    garden_orientation = fields.Selection(
+        selection=[
+            ("north", "North"),
+            ("south", "South"),
+            ("east", "East"),
+            ("west", "West"),
+        ]
+    )
+    active = fields.Boolean(default=True)
+    status = fields.Selection(
+        selection=[
+            ("new", "New"),
+            ("offer_received", "Offer Received"),
+            ("offer_accepted", "Offer Accepted"),
+            ("sold", "Sold"),
+            ("canceled", "Canceled"),
+        ],
+        copy=False,
+        default="new",
+        required=True,
+    )
