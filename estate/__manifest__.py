5601f143e0c7124fa7edd093d34fae8da684db5f
Alberto Larios <albertolarios@me.com>
2023-04-24 16:24:02 -0600

    [IMP] estate: add views and new models and associated relations T#71276
    - Add determined fields to model's inline view
    - Add basic view form to model's entries
    - Add tab within view form for model's entries
    - Add available filter, and aggregation by postcode
    - Add PropertyType model and set associated permissions
    - Add PropertyType tree view to the menu and add customized form view
    - Add PropertyType to Property field and add it to views
    - Add buyer and salesperson fields to Property model and form view
    - Add PropertyTag model and set associated permissions
    - Add PropertyTag action to the menu, under "Settings"
    - Add PropertyTag to Property field and add it to its views
    - Add PropertyOffer model and set associated permissions
    - Add form and tree views to PropertyOffer model
    - Link PropertyOffer model to EstateProperty and add tree view

--- estate/__manifest__.py
+++ estate/__manifest__.py
@@ -5,5 +5,12 @@
     "author": "Vauxoo",
     "depends": [
         "base",
-    ]
+    ],
+    "data": [
+        "security/ir.model.access.csv",
+        "views/estate_property_views.xml",
+        "views/estate_property_type_views.xml",
+        "views/estate_property_tag_views.xml",
+        "views/estate_menus.xml",
+    ],
 }
--- estate/models/estate_property.py
+++ estate/models/estate_property.py
@@ -8,18 +8,38 @@ class EstateProperty(models.Model):
     name = fields.Char(required=True)
     description = fields.Text()
     postcode = fields.Char()
-    date_availability = fields.Date()
+    date_availability = fields.Date(copy=False, default=lambda self: fields.Date.add(fields.Date.today(), months=3))
     expected_price = fields.Float(required=True)
-    selling_price = fields.Float()
-    bedrooms = fields.Integer()
+    selling_price = fields.Float(copy=False, readonly=True)
+    bedrooms = fields.Integer(default=2)
     living_area = fields.Integer()
     facades = fields.Integer()
     garage = fields.Boolean()
     garden = fields.Boolean()
     garden_area = fields.Integer()
-    garden_orientation = fields.Selection([
-        ("north", "North"),
-        ("south", "South"),
-        ("east", "East"),
-        ("west", "West")
-    ])
+    garden_orientation = fields.Selection(
+        selection=[
+            ("north", "North"),
+            ("south", "South"),
+            ("east", "East"),
+            ("west", "West"),
+        ]
+    )
+    active = fields.Boolean(default=True)
+    status = fields.Selection(
+        selection=[
+            ("new", "New"),
+            ("offer_received", "Offer Received"),
+            ("offer_accepted", "Offer Accepted"),
+            ("sold", "Sold"),
+            ("canceled", "Canceled"),
+        ],
+        copy=False,
+        default="new",
+        required=True,
+    )
+    property_type_id = fields.Many2one(comodel_name="estate.property.type", string="Property Type")
+    buyer = fields.Many2one("res.partner", copy=False)
+    salesperson = fields.Many2one("res.users", default=lambda self: self.env.user)
+    tag_ids = fields.Many2many("estate.property.tag", string="Tags")
+    offer_ids = fields.One2many("estate.property.offer", "property_id", String="Offers")
