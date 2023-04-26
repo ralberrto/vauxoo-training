cefd72001f1dfe9db59540bfff06b7a066c57778
Alberto Larios <albertol@vauxoo.com>
2023-04-26 16:54:11 -0600

    [IMP] estate: add views and models with associated relationships T#71276
    - Add determined fields to model's inline view
    - Add basic view form to model's entries
    - Add tab within view form for model's entries
    - Add available filter, and aggregation by postcode
    - Add PropertyType model and set associated permissions
    - Add PropertyType tree view to the menu and add customized form view
    - Add PropertyType to Property field and add it to views
    - Add buyer and salesperson fields to Property model and form view
    - Add PropertyTag model and set associated permissions
    - Add PropertyTag action to the menu, under "Settings"
    - Add PropertyTag to Property field and add it to its views
    - Add PropertyOffer model and set associated permissions
    - Add form and tree views to PropertyOffer model
    - Link PropertyOffer model to EstateProperty and add tree view

--- estate/models/estate_property.py
+++ estate/models/estate_property.py
@@ -5,18 +5,20 @@ class EstateProperty(models.Model):
     _name = "estate.property"
     _description = "Estate Property"
 
-    name = fields.Char(required=True)
+    name = fields.Char(required=True, string="Title")
     description = fields.Text()
     postcode = fields.Char()
-    date_availability = fields.Date(copy=False, default=lambda self: fields.Date.add(fields.Date.today(), months=3))
+    date_availability = fields.Date(
+        copy=False, default=lambda self: fields.Date.add(fields.Date.today(), months=3), string="Available From"
+    )
     expected_price = fields.Float(required=True)
     selling_price = fields.Float(copy=False, readonly=True)
     bedrooms = fields.Integer(default=2)
-    living_area = fields.Integer()
+    living_area = fields.Integer(string="Living Area (sqm)")
     facades = fields.Integer()
     garage = fields.Boolean()
     garden = fields.Boolean()
-    garden_area = fields.Integer()
+    garden_area = fields.Integer("Garden Area (sqm)")
     garden_orientation = fields.Selection(
         selection=[
             ("north", "North"),
@@ -38,3 +40,8 @@ class EstateProperty(models.Model):
         default="new",
         required=True,
     )
+    property_type_id = fields.Many2one(comodel_name="estate.property.type")
+    buyer_id = fields.Many2one("res.partner", copy=False)
+    salesperson_id = fields.Many2one("res.users", default=lambda self: self.env.user, string="Salesman")
+    tag_ids = fields.Many2many("estate.property.tag", string="Tags")
+    offer_ids = fields.One2many("estate.property.offer", "property_id", string="Offers")
