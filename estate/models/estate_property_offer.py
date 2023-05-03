397ea0a2b68c4969476bae5d9a9a183577eb8fef
Alberto Larios <albertol@vauxoo.com>
2023-05-03 12:26:52 -0600

    [IMP] estate: add computed fields and onchange events T#71276
    - Add total_area computed field and include it in form view
    - Add best_price computed field and include it in form view
    - Add interdependent (computed and inverted) fields to model and display them
    - Add onchange event to garden field
    - Add actions to change status in EstateProperty model
    - Compute selling_price and buyer_id fields based on EstatePropertyOffer model

--- estate/models/estate_property_offer.py
+++ estate/models/estate_property_offer.py
@@ -1,4 +1,4 @@
-from odoo import fields, models
+from odoo import api, exceptions, fields, models
 
 
 class EstatePropertyOffer(models.Model):
@@ -15,3 +15,29 @@ class EstatePropertyOffer(models.Model):
     )
     partner_id = fields.Many2one("res.partner", required=True)
     property_id = fields.Many2one("estate.property", required=True)
+    validity = fields.Integer(default=7, string="Validity (days)")
+    date_deadline = fields.Date(compute="_compute_date_deadline", inverse="_inverse_date_deadline", string="Deadline")
+
+    @api.depends("validity")
+    def _compute_date_deadline(self):
+        for record in self:
+            if not record.create_date:
+                create_date = fields.Date.today()
+            else:
+                create_date = record.create_date.date()
+            record.date_deadline = fields.Date.add(create_date, days=record.validity)
+
+    def _inverse_date_deadline(self):
+        for record in self:
+            record.validity = (record.date_deadline - record.create_date.date()).days
+
+    def action_set_status_accepted(self):
+        for record in self:
+            if "accepted" in record.property_id.offer_ids.mapped("status"):
+                raise exceptions.UserError("Only one offer may be accepted.")
+            record.status = "accepted"
+            record.property_id.buyer_id = record.partner_id
+            record.property_id.selling_price = record.price
+
+    def action_set_status_refused(self):
+        self.write({"status": "refused"})
