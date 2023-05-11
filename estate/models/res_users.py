017e7fd5b576d2c2fad0fe9f793ba4a442b097e9
Alberto Larios <albertol@vauxoo.com>
2023-05-11 16:55:43 -0600

    Add Users model inheriting from res.users

---
+++ estate/models/res_users.py
@@ -0,0 +1,10 @@
+from odoo import fields, models
+
+
+class Users(models.Model):
+    _name = "res.users"
+    _inherit = "res.users"
+
+    property_ids = fields.One2many(
+        "estate.property", "salesperson_id", domain="[('status', 'in', ['new', 'offer_received'])]"
+    )
