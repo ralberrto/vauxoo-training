0f79a3c7de93f87b35faa6646d76a07b86e1a8ea
Alberto Larios <albertol@vauxoo.com>
2023-05-15 12:52:45 -0600

    Create invoice with the create_invoice method from sale when a property is marked as sold

--- estate_account/models/estate_property.py
+++ estate_account/models/estate_property.py
@@ -1,4 +1,5 @@
 from odoo import models
+from odoo.fields import Command
 
 
 class EstateProperty(models.Model):
@@ -6,4 +7,27 @@ class EstateProperty(models.Model):
     _inherit = "estate.property"
 
     def action_set_status_sold(self):
+        for record in self:
+            self.env["account.move"].create(
+                {
+                    "partner_id": record.buyer_id.id,
+                    "move_type": "out_invoice",
+                    "line_ids": [
+                        Command.create(
+                            {
+                                "name": record.name,
+                                "quantity": 1,
+                                "price_unit": record.selling_price * 0.06,
+                            }
+                        ),
+                        Command.create(
+                            {
+                                "name": "Administrative fees",
+                                "quantity": 1,
+                                "price_unit": 100,
+                            }
+                        ),
+                    ],
+                }
+            )
         return super().action_set_status_sold()
